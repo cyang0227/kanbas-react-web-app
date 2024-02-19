77c17ab2480b716041827b35c35ee65a4cadcf77
cyang0227 <cyang0227@gmail.com>
2024-02-19 18:49:27 -0500

    update

--- src/Labs/a3/Add.tsx
+++ src/Labs/a3/Add.tsx
@@ -4,7 +4,7 @@ function Add({ a, b }: { a: number, b: number }) {
             <h3>Add</h3>
             a = {a} <br />
             b = {b} <br />
-            a + b = {a + b}
+            a + b = {a + b} <br />
         </>
     );
 }
