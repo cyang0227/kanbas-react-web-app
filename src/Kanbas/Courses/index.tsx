5725b53b19b4bdc692124fa7f360b0968920dd18
cyang0227 <yang.chen8@northeastern.edu>
2024-02-20 10:41:37 -0500

    update

--- src/Kanbas/Courses/index.tsx
+++ src/Kanbas/Courses/index.tsx
@@ -2,13 +2,14 @@ import { courses } from "../../Kanbas/Database";
 import { Navigate, Route, Routes, useParams } from "react-router-dom";
 import { HiMiniBars3 } from "react-icons/hi2";
 import CourseNavigation from "./Navigation";
+import Modules from "./Modules";
 
 function Courses() {
   const { courseId } = useParams();
   const course = courses.find((course) => course._id === courseId);
   return (
     <div>
-      <h1><HiMiniBars3 /> Course {course?.name}</h1>
+      <h1><HiMiniBars3 /> {course?.name}</h1>
       <CourseNavigation />
       <div>
         <div
@@ -17,7 +18,7 @@ function Courses() {
           <Routes>
             <Route path="/" element={<Navigate to="Home" />} />
             <Route path="Home" element={<h1>Home</h1>} />
-            <Route path="Modules" element={<h1>Modules</h1>} />
+            <Route path="Modules" element={<Modules/>} />
             <Route path="Piazza" element={<h1>Piazza</h1>} />
             <Route path="Assignments" element={<h1>Assignments</h1>} />
             <Route path="Assignments/:assignmentId" element={<h1>Assignment Editor</h1>} />
