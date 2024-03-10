2c2c43c7bd14a1607854615e00cd7f3666e2d9b7
cyang0227 <cyang0227@gmail.com>
2024-03-10 09:30:56 -0400

    update

--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -1,18 +1,22 @@
 import Assignment3 from "./a3";
 import Assignment4 from "./a4";
-import Nav from "../nav"
+import Nav from "../nav";
 import { Routes, Route, Link } from "react-router-dom";
+import store from "./store";
+import { Provider } from "react-redux";
 //import Assignment4 from "./a4";
 function Labs() {
   return (
-    <div className="container-fluid">
-      <h1>Labs</h1>
-      <Nav />
-      <Routes>
-        <Route path="/a3/*" element={<Assignment3 />} />
-        <Route path="/a4/*" element={<Assignment4 />} />
-      </Routes>
-    </div>
+    <Provider store={store}>
+      <div className="container-fluid">
+        <h1>Labs</h1>
+        <Nav />
+        <Routes>
+          <Route path="/a3/*" element={<Assignment3 />} />
+          <Route path="/a4/*" element={<Assignment4 />} />
+        </Routes>
+      </div>
+    </Provider>
   );
 }
 export default Labs;
