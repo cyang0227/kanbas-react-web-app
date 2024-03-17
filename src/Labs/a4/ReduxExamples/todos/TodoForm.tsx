4e31d1d15d04e16ae1f7b9d052964c65872299ea
cyang0227 <cyang0227@gmail.com>
2024-03-17 00:23:35 -0400

    update

--- src/Labs/a4/ReduxExamples/todos/TodoForm.tsx
+++ src/Labs/a4/ReduxExamples/todos/TodoForm.tsx
@@ -1,18 +1,20 @@
-function TodoForm({ todo, setTodo, addTodo, updateTodo }: {
-    todo: { id: string; title: string };
-    setTodo: (todo: { id: string; title: string }) => void;
-    addTodo: (todo: { id: string; title: string }) => void;
-    updateTodo: (todo: { id: string; title: string }) => void;
-  }) {
-    return (
-      <li className="list-group-item">
-        <button onClick={() => addTodo(todo)}> Add </button>
-        <button onClick={() => updateTodo(todo)}> Update </button>
-        <input
-          value={todo.title}
-          onChange={ (e) => setTodo({ ...todo, title: e.target.value }) }
-        />
-      </li>
-    );
-  }
-  export default TodoForm;
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { addTodo, updateTodo, setTodo } from "./todosReducer";
+import { LabState } from "../../../store";
+
+function TodoForm() {
+  const { todo } = useSelector((state: LabState) => state.todosReducer);
+  const dispatch = useDispatch();
+  return (
+    <li className="list-group-item">
+      <button className="btn btn-primary" onClick={() => dispatch(addTodo(todo))}> Add </button>
+      <button className="btn btn-success" onClick={() => dispatch(updateTodo(todo))}> Update </button>
+      <input
+        value={todo.title}
+        onChange={(e) => dispatch(setTodo({ ...todo, title: e.target.value })) }
+      />
+    </li>
+  );
+}
+export default TodoForm;
