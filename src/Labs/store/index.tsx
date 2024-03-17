6cfa480d72925e1c13285fe2ca897944b900a85d
cyang0227 <cyang0227@gmail.com>
2024-03-17 00:36:29 -0400

    update

--- src/Labs/store/index.tsx
+++ src/Labs/store/index.tsx
@@ -2,7 +2,7 @@ import { configureStore } from "@reduxjs/toolkit";
 import helloReducer from "../a4/ReduxExamples/HelloRedux/helloReducer";
 import counterReducer from "../a4/ReduxExamples/CounterRedux/counterReducer";
 import addReducer from "../a4/ReduxExamples/AddRedux/addReducer";
-import todoReducer from "../a4/ReduxExamples/todos/todosReducer";
+import todosReducer from "../a4/ReduxExamples/todos/todosReducer";
 
 export type TodoType = {
     id: string;
@@ -29,7 +29,7 @@ const store = configureStore({
         helloReducer,
         counterReducer,
         addReducer,
-        todoReducer,
+        todosReducer,
     },
 });
 export default store;
