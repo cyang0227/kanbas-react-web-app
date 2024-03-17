fddb85001c1197701470172a0a8f34eb0831673c
cyang0227 <cyang0227@gmail.com>
2024-03-17 02:09:52 -0400

    update

--- src/Kanbas/index.tsx
+++ src/Kanbas/index.tsx
@@ -2,7 +2,43 @@ import KanbasNavigation from "./Navigation";
 import { Routes, Route, Navigate } from "react-router-dom";
 import Dashboard from "./Dashboard";
 import Courses from "./Courses";
+import { useState } from "react";
+import { courses } from "./Database";
+
 function Kanbas() {
+
+  const [_courses, setCourses] = useState<any[]>(courses);
+  const [course, setCourse] = useState({
+    _id: "0",
+    name: "New Course",
+    number: "New Number",
+    startDate: "2023-09-10",
+    endDate: "2023-12-15",
+    image: "/images/reactjs.jpg",
+  });
+
+  const addNewCourse = () => {
+    const newCourse = { ...course, _id: new Date().getTime().toString() };
+    setCourses([..._courses, { ...course, ...newCourse }]);
+  };
+
+  const deleteCourse = (courseID: string) => {
+    setCourses(_courses.filter((course) => course._id !== courseID));
+  };
+
+  const updateCourse = () => {
+    setCourses(
+      courses.map((c) => {
+        if (c._id === course._id) {
+          return course;
+        } else {
+          return c;
+        }
+      })
+    );
+  };
+
+
   return (
     <div className="d-flex">
       <KanbasNavigation />
@@ -10,8 +46,17 @@ function Kanbas() {
       <Routes>
           <Route path="/" element={<Navigate to="Dashboard" />} />
           <Route path="Account" element={<h1>Account</h1>} />
-          <Route path="Dashboard" element={<Dashboard />} />
-          <Route path="Courses/:courseId/*" element={<Courses />} />
+          <Route path="Dashboard" element={<Dashboard 
+            courses={_courses} 
+            course={course} 
+            setCourse={setCourse} 
+            addNewCourse={addNewCourse} 
+            deleteCourse={deleteCourse} 
+            updateCourse={updateCourse}
+          />} />
+          <Route path="Courses/:courseId/*" element={
+          <Courses _courses={courses}
+          />} />
         </Routes>
       </div>
     </div>
