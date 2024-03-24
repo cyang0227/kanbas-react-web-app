a8c0a55566d6b45ec6962bd9c507ff0b3782bb72
cyang0227 <cyang0227@gmail.com>
2024-03-24 14:38:29 -0400

    update

--- src/Labs/a4/ReduxExamples/todos/TodoItem.tsx
+++ src/Labs/a4/ReduxExamples/todos/TodoItem.tsx
@@ -1,16 +1,14 @@
-import React from "react";
-import { useDispatch } from "react-redux";
-import { deleteTodo, setTodo } from "./todosReducer";
-import { LabState, TodoType } from "../../../store";
-
-function TodoItem( todo: TodoType) {
-  const dispatch = useDispatch();
-  return (
-    <li key={todo.id} className="list-group-item">
-      <button className="btn btn-danger" onClick={() => dispatch(deleteTodo(todo.id)) }> Delete </button>
-      <button className="btn btn-warning" onClick={() => dispatch(setTodo(todo))}> Edit </button>
-      {todo.title}
-    </li>
-  );
-}
-export default TodoItem;
+function TodoItem({ todo, deleteTodo, setTodo }: {
+    todo: { id: string; title: string };
+    deleteTodo: (id: string) => void;
+    setTodo: (todo: { id: string; title: string }) => void;
+  }) {
+    return (
+      <li key={todo.id} className="list-group-item">
+        <button onClick={() => deleteTodo(todo.id)}> Delete </button>
+        <button onClick={() => setTodo(todo)}> Edit </button>
+        {todo.title}
+      </li>
+    );
+  }
+  export default TodoItem;
--- src/Labs/a4/ReduxExamples/todos/TodoList.tsx
+++ src/Labs/a4/ReduxExamples/todos/TodoList.tsx
@@ -3,17 +3,17 @@ import TodoForm from "./TodoForm";
 import TodoItem from "./TodoItem";
 function TodoList() {
   const [todos, setTodos] = useState([
-    { id: 1, title: "Learn React" },
-    { id: 2, title: "Learn Node" },
+    { id: "1", title: "Learn React" },
+    { id: "2", title: "Learn Node" },
   ]);
-  const [todo, setTodo] = useState({ id: -1, title: "Learn Mongo" });
+  const [todo, setTodo] = useState({ id: "-1", title: "Learn Mongo" });
   const addTodo = (todo: any) => {
     const newTodos = [
       ...todos,
       { ...todo, id: new Date().getTime().toString() },
     ];
     setTodos(newTodos);
-    setTodo({ id: -1, title: "" });
+    setTodo({ id: "-1", title: "" });
   };
   const deleteTodo = (id: any) => {
     const newTodos = todos.filter((todo) => todo.id !== id);
@@ -22,42 +22,20 @@ function TodoList() {
   const updateTodo = (todo: any) => {
     const newTodos = todos.map((item) => (item.id === todo.id ? todo : item));
     setTodos(newTodos);
-    setTodo({ id: -1, title: "" });
+    setTodo({ id: "-1", title: "" });
   };
   return (
     <div>
       <h2>Todo List</h2>
       <ul className="list-group">
-        <li className="list-group-item">
-          <button className="btn btn-primary" onClick={() => addTodo(todo)}>
-            Add
-          </button>
-          <button
-            className="btn btn-secondary"
-            onClick={() => updateTodo(todo)}
-          >
-            Update
-          </button>
-          <input
-            className="form-control"
-            type="text"
-            value={todo.title}
-            onChange={(e) => setTodo({ ...todo, title: e.target.value })}
-          />
-        </li>
+        <TodoForm
+          todo={todo}
+          setTodo={setTodo}
+          addTodo={addTodo}
+          updateTodo={updateTodo}
+        />
         {todos.map((todo) => (
-          <li key={todo.id} className="list-group-item">
-            <button
-              className="btn btn-danger"
-              onClick={() => deleteTodo(todo.id)}
-            >
-              Delete
-            </button>
-            <button className="btn btn-secondary" onClick={() => setTodo(todo)}>
-              Edit
-            </button>
-            {todo.title}
-          </li>
+          <TodoItem todo={todo} deleteTodo={deleteTodo} setTodo={setTodo} />
         ))}
       </ul>
     </div>
